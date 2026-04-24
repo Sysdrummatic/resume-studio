5b0662e7427cfd971282e5225c71b7ca7e38aa4a
LukaszMichta <lukasz.michta@cdq.com>
2026-04-25 01:29:28 +0200

    Show account menu on home route

--- app/components/header-account-menu.tsx
+++ app/components/header-account-menu.tsx
@@ -1,16 +1,13 @@
 "use client";
 
 import { ReactNode } from "react";
-import { usePathname } from "next/navigation";
 
 type Props = {
   actor: ReactNode;
 };
 
 export default function HeaderAccountMenu({ actor }: Props) {
-  const pathname = usePathname();
-
-  if (!actor || pathname === "/") {
+  if (!actor) {
     return null;
   }
 
